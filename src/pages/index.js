d29fc1b1e6258f5b81d9835303ee233c9c32e58a
Fabian-Tech <63789547+Fabian-Tech@users.noreply.github.com>
2020-04-17 13:14:56 +0200

    Add files via upload

---
+++ src/pages/index.js
@@ -0,0 +1,11 @@
+import React from "react"
+import Car from '../App'
+
+export default () => (
+    <div style={{ color: `purple`}}>
+        <h1>Hello Gatsby!</h1>
+        <p>What a world.</p>
+        <img src="https://source.unsplash.com/random/400x200" alt="" />
+        <Car/>
+    </div>
+)
